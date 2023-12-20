08d42dfa453e20fb46188162391122cb88f9ffe2
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-20 13:14:16 -0700

    asd

--- src/App/components/ImagePlayer.js
+++ src/App/components/ImagePlayer.js
@@ -1,34 +1,80 @@
 import { useEffect, useState } from "react";
+import Alert from '@mui/material/Alert';
 
 export default function ImagePlayer({streamFile, videoRef}){
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
-        if (!streamFile || !videoRef) return;
+        if (!streamFile || !videoRef || !videoRef.current) return;
         let timeoutId = null;
-        let lastUpdateTime=(new Date()).getTime();
         const video=videoRef.current;
+        let lastBlob=null;
+        let lastTime=(new Date()).getTime();
+        let cancelling = false;
 
-        function updateImage(){
-            video.src='/api/cam/'+streamFile?.file+'?'+(new Date().getTime());
+        async function updateImage(){
+            try {
+                const options={
+                    credentials: 'include',
+                    method: "GET",
+                    cache: "no-cache",
+                }
+                const response = await fetch('/api/cam/'+streamFile?.file, options);
+                const blob = await response.blob();
+                const newBlob = URL.createObjectURL(blob);
+                video.src=newBlob;
+                try{
+                    if (lastBlob) URL.revokeObjectURL(lastBlob);
+                }catch(e){
+                    console.error(e);
+                }
+                lastBlob=newBlob;
+                return true;
+            }catch(e){
+                console.error(e);
+                return false;
+            }
         }
 
-        function onLoad(){
-            const currentTime = (new Date()).getTime();
-            const loadOffset = currentTime-lastUpdateTime;
-            lastUpdateTime=currentTime;
-            timeoutId=setTimeout(updateImage, Math.max(0, 1/streamFile.fps*1000 - loadOffset));
+        async function loadNext(){
+            const beforeTime = (new Date()).getTime();
+            console.log(beforeTime-lastTime);
+            lastTime=beforeTime;
+            const success = await updateImage();
+            const afterTime = (new Date()).getTime();
+            const loadOffset = afterTime-beforeTime;
+            if (cancelling) return;
+            const period = Math.max(0, 1/streamFile.fps*1000 - loadOffset);
+            if (period<=0){
+                setError({type: 'warning', text: 'network is slower than the servers frame rate'});
+            }else if (!success){
+                setError({type: 'error', text: 'failed to fetch latest image'});
+            }else{
+                setError(null);
+            }
+            timeoutId=setTimeout(loadNext, period);
         }
-        timeoutId=setTimeout(updateImage, 1/streamFile.fps*1000);
 
-        video.addEventListener('load', onLoad);
+        loadNext();
 
         return ()=>{
-            video.removeEventListener('load', onLoad);
+            cancelling=true;
+            try{
+                if (lastBlob) URL.revokeObjectURL(lastBlob);
+            }catch{}
             if (timeoutId){
                 clearTimeout(timeoutId);
             }
         }
     }, [streamFile, videoRef]);
 
-    return <img style={{width:'100%', height:'100%'}} ref={videoRef} src={'/api/cam/'+streamFile?.file} alt=''></img>;
+    return <>
+        {
+            error?
+                <Alert severity={error.type}>{error.text}</Alert>
+            :
+                null
+        }
+        <img style={{width:'100%', height:'100%'}} ref={videoRef} alt=''></img>
+    </>
 }
